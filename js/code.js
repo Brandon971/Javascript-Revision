d7ec0432aa4960c305a0372a2d4065c67af5161a
Brandon971 <r.swinton971@gmail.com>
2022-11-30 16:39:29 +0200

    update

--- js/code.js
+++ js/code.js
@@ -68,7 +68,7 @@
 //     console.log("YOU A JOE ");
 // }
 
-//logical operater are &&(and) , ||(or) , !(not)
+//logical operater are &&(and) , ||(or) , !(not);
 
 //Switch statement
 // let monthInt = 1;
@@ -80,22 +80,215 @@
 //         console.log("february");
 //     break;
 // }
-let grade = 100 ;
-switch(true) {
-    case grade == 100 : console.log("your awesome")
-    break;
-    case (grade >= 90) && (grade <= 99): 
-        console.log("well done")
-    break;
-    case (grade >= 75) && (grade <= 89): 
-        console.log("distinction")
-    break;
-    case (grade >= 50) && (grade <=74): 
-        console.log("Passed")
-    break;
-    case (grade <= 49) && (Math.sign(grade) !=-1 ) : 
-        console.log("Failed")
-    break;
-    default:
-        console.log("fake!")
-}
+// let grade = 100 ;
+// switch(true) {
+//     case grade == 100 : console.log("your awesome")
+//     break;
+//     case (grade >= 90) && (grade <= 99 ): 
+//         console.log("well done")
+//     break;
+//     case (grade >= 75) && (grade <= 89): 
+//         console.log("distinction")
+//     break;
+//     case (grade >= 50) && (grade <=74): 
+//         console.log("Passed")
+//     break;
+//     case (grade <= 49) && (Math.sign(grade) !=-1 ) : 
+//         console.log("Failed")
+//     break;
+//     default:
+//         console.log("fake!")
+// }
+
+//loops
+/*for in (will give the index)
+for of
+for(; ;)
+while
+do while
+.forEach()
+*/
+// let numbers =[9, 7, 8, 9, 4]
+// let lovechild ={
+//     name:'Timothy',
+//     surname: 'Mgoqi',
+//     email: 'timothy@gmail.com'
+// }
+//object for in
+// for (let love in lovechild){
+//     console.log(`${love} => ${lovechild[love]}`)
+// }
+// for(let numb in numbers ) {
+//     console.log(numb);
+// }
+// console.table(numbers);
+
+//for (; ;)
+// for(let i=0 ;;) {
+//     console.log("let found out")
+// }
+
+//while loop 
+// let cnt = 0;
+// while(cnt < numbers.length) {
+//     console.log(`Step:${number[cnt]}`);
+////     cnt++;
+// }
+
+//do .. while
+// do{
+//     console.log(`Step: ${numbers[cnt]}`);
+//     cnt++;
+// }while(cnt < numbers.length);
+
+//function 
+// let numb1 = prompt("enter nommer");
+// let numb2 = prompt("enter nommer jy")
+// function addition (numb1 , numb2) {
+//     console.log("Sum is: ",numb1 + numb2);
+// }
+// addition(4, 5);
+// addition(5, 7);
+// addition(9, 23);
+
+//sum with an arrow function
+// let addition = (numb1, numb2) => {
+//     console.log(numb1 + numb2);
+// }
+// addition(5, 7)
+//return a sum with a function
+// function subtraction(number1 , number2) {
+// return number1 - number2 
+
+// };
+// console.log(subtraction(10, 5));
+
+//shortcut but only calls it once
+// ((numb1, numb2)=>{
+//     console.log(numb1+numb2);
+// })(8, 2);
+// Ternary operater
+// let age = prompt("enter age");
+// console.log( age >= 18 ? "Big balls" : "Still a baby");
+
+//rest operator
+// function sumOf(...args) {
+//     return args.reduce( (a, b)=>{
+//         return a + b
+//     });
+// }
+// console.log(sumOf(1,2,3,4,5));
+
+//Constructor function 
+// (can only place a negative number on a "at" method)
+// function Person(...details) {
+//     this.firstName = details[0];
+//     this.lastName = details[1];
+//     this.email = details.at(-1);
+// }
+// let person1 = new Person("Franky","Deluxe","peter@gmail.com")
+// console.log(person1)
+// function PersonDetails(firstName, lastName, 
+//     email) {
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//     this.email = email;
+
+//     this.getFirstName = ()=> {
+//         return this.firstName
+//     }
+//     this.setFirstName = (value) =>{
+//         this.firstName = value;
+//     }
+//     this.getLastName = ()=> {
+//         return this.lastName
+//     }
+//     this.setLastName = (value) =>{
+//         this.lastName = value;
+//     }
+//     this.getEmail = ()=> {
+//         return this.lastName
+//     }
+//     this.setEmail = (value) =>{
+//         this.email = value;
+//     }    
+    
+//     this.display = ()=> {
+//         return `
+//         Name: ${this.getFirstName()}
+//         Surname: ${this.getLastName()}
+//         Email: ${this.getEmail()}`;
+//     }
+// } 
+// let person1 = new PersonDetails("Peter", "Henk",
+// "peter@gmail.com");
+// console.log(person1.display());
+// console.log("=============");
+// person1.setFirstName("Sarah");
+// person1.setLastName("James");
+// person1.setEmail("sarah@gmail.com");
+// console.log(person1.display());
+
+//Solution 2 using a class
+// class PersonDetails{
+//     // Private
+//     #firstName;
+//     #lastName;
+//     #email;
+//     constructor(firstName, lastName, email) {
+//         this.#firstName = firstName;
+//         this.#lastName = lastName;
+//         this.#email = email;
+//     }
+//     // FirstName
+//     get firstName() {
+//         return this.#firstName;
+//     }
+//     set firstName(value) {
+//         this.#firstName = value;
+//     }
+//     // LastName
+//     get lastName() {
+//         return this.#lastName;
+//     }
+//     set lastName(value) {
+//         this.#lastName = value;
+//     }
+//     // Email
+//     get email() {
+//         return this.#email;
+//     }
+//     set email(value) {
+//         this.#email = value;
+//     }
+//     // Display
+//     display() {
+//         console.log(`
+//         Name: ${this.firstName}
+//         Surname: ${this.lastName}
+//         Email: ${this.email}`);
+//     }
+// }
+// let person1 = new PersonDetails("Joel", "Mukanya", "joel@gmail.com");
+// person1.display();
+// console.log("===========");
+// person1.firstName = "Peter";
+// person1.lastName = "Henk";
+// person1.email = "peter@gmail.com"
+// person1.display();
+
+//ONCLICK
+// function addition(e) {
+//     e.preventDefault();
+//     let numb1 = document.querySelector("#numb1").value;
+//     let numb2 = document.querySelector("#numb2").value;
+//     let output = document.querySelector('#output').innerText = eval(`${numb1} + ${numb2}`)
+// }
+
+//addEvenListener
+let btnAddition = document.querySelector('button');
+btnAddition.addEventListener('click',() =>{
+    let numb1 = document.querySelector("numb1").value;
+    let numb2 = document.querySelector("numb2").value;
+    let output = document.querySelector("#output").innerText = parseInt(numb1)+ parseInt(numb2)
+}) 
